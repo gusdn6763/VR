12e137465909bef7ab11346dd351f25e99af672b
gusdn6763 <gusdn6763@gmail.com>
2021-04-14 13:24:45 +0900

    dot player fix

--- HelloUnity/Assets/Scripts/Rotator.cs
+++ HelloUnity/Assets/Scripts/Rotator.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float rotation = 60f;
 
-    private void Update()
+    public float rotationSpeed = 60f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        if (!GameManager.instance.isGameover)
-        {
-            rotation += 0.01f;
-            transform.Rotate(0f, (rotation * Time.deltaTime), 0f);
-        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
 }
